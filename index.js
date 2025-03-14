cc5c9a192f219d21b7b6b3e68153fc2448df0d07
Mercy <maritimmercy8@gmail.com>
2025-03-14 23:25:43 +0300

    added colors

--- index.js
+++ index.js
@@ -24,8 +24,85 @@ const nestedMuppet = {
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
 };
+// An array of animal sounds
+const animalSounds = ["cow", "horse", "sheep", "pig", "chicken"];
 
+// Destructuring the array into variables
+const [moo, neigh, baa, oink, cluck] = animalSounds;
+
+// Logging to verify
+console.log(moo);   // "cow"
+console.log(neigh); // "horse"
+console.log(baa);   // "sheep"
+console.log(oink);  // "pig"
+console.log(cluck); // "chicken"
+
+// Array of animal names
+const animals = ["cow", "sheep", "pig", "chicken"];
+
+// Destructuring to declare variables
+const [bessie, dolly, babe, little] = animals;
+
+console.log(bessie); // "cow"
+console.log(dolly);  // "sheep"
+console.log(babe);   // "pig"
+console.log(little); // "chicken"
+
+// Array of animals corresponding to their colors
+const animalColors = ["cow", "sheep", "pig"];
+
+// Destructuring to declare variables for colors
+const [blackAndWhite, black, pink] = animalColors;
+
+console.log(blackAndWhite); // "cow"
+console.log(black);          // "sheep"
+console.log(pink);           // "pig"
+
+// Array of rainbow colors
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
+console.log(red);   // "red"
+console.log(orange); // "orange"
+console.log(yellow); // "yellow"
+console.log(green);  // "green"
+console.log(blue);   // "blue"
+console.log(indigo); // "indigo"
+console.log(violet); // "violet"
+
+// Object destructuring
+// Array of rainbow colors
+const rainbowColors = ["red", "orange", "yellow", "green", "blue", "violet"];
+
+// Destructuring to declare variables using initials
+const [r, o, y, g, b, v] = rainbowColors;
+
+console.log(r); // "red"
+console.log(o); // "orange"
+console.log(y); // "yellow"
+console.log(g); // "green"
+console.log(b); // "blue"
+console.log(v); // "violet"
+
+
+// Array of rainbow colors
+const colorsOfRainbow = ["red", "orange", "yellow", "green", "blue", "violet", "indigo"];
+
+// Destructuring to declare indg for "indigo"
+const [, , , , , , indg] = rainbowColors;
+
+console.log(indg); // "indigo"
 // Strings
+
+
+const muppetName = "Miss Piggy";
+const color = "pink";
+const song = "Never Before, Never Again";
+const job = "Cast member of The Muppet Show";
+const partner = "Kermit";
+
+const song2 = "Moving Right Along";
+const song4 = "I Hope That Something Better Comes Along";
+const nestedJob = "Host of The Muppet Show";
+const nestedPartner = "Miss Piggy";
 
 // 1. Use destructuring to assign appropriate variables based on the sounds animals make.
 
